2469394f389d4cdc89a27f44c887067aae3d71ae
이주호 <juho981101@naver.com>
2023-03-28 14:16:06 +0900

    'commit'

--- 고득점 Kit/04_해시(Hash)/03_전화번호 목록.py
+++ 고득점 Kit/04_해시(Hash)/03_전화번호 목록.py
@@ -1,11 +1,8 @@
-# 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false
-# 어떤 번호가 다른 번호의 접두어인 경우가 없으면 True
-
-# phone_book의 길이는 1이상 1,000,000 이하
-# 전화번호의 길이는 1이상 20이하 
-# 중복 x
-
 def solution(phone_book):
-
-phone_book = ["123","456","789"]
-print(solution(phone_book))
+    
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1) :
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])] :
+            return False 
+    return True
