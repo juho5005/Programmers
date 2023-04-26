84c52debdbc4d60aa5a952710d6247b4e6201262
이주호 <juho981101@naver.com>
2023-04-26 13:21:04 +0900

    'commit'

---
+++ Lv_1/01_달리기 경주.py
@@ -0,0 +1,21 @@
+def solution(players, callings):
+    # 선수들의 이름과 그 당시 인덱스를
+    # (key, value)로 저장해두기
+    dic = {}
+
+    for idx, player in enumerate(players) :
+        dic[player] = idx 
+    
+    for calling in callings : 
+        pos = dic[calling]
+
+        dic[players[pos]] = pos-1 
+        dic[players[pos-1]] = pos
+
+        players[pos-1], players[pos] = players[pos], players[pos-1]
+
+    return players
+
+players = ["mumu", "soe", "poe", "kai", "mine"]
+callings = ["kai", "kai", "mine", "mine"]
+print(solution(players, callings))
